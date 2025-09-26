3d82eb5f7536524b945ef2e475df83c022beff0a
AlejandroDel <alejandrohdzd@proton.me>
2025-09-25 21:04:06 -0600

    Update task1.js

---
+++ task1.js
@@ -0,0 +1,9 @@
+function cambioPropiedadesTexto (){
+
+    let texto = document.getElementById('texto')
+    texto.innerHTML = "Texto2";
+    texto.style.color = "green";
+    texto.style.fontSize = "50px";
+
+
+}
